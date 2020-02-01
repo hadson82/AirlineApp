1b1b96153b1afd756f0cdb1c1adbefebba011a47
Pavel <rogozhin.p@yandex.ru>
2020-02-01 22:44:06 +0300

    persisting passenger object to database

--- web/src/com/airline/controllers/AddPassenger.java
+++ web/src/com/airline/controllers/AddPassenger.java
@@ -1,31 +1,30 @@
 package com.airline.controllers;
 
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
-import javax.servlet.RequestDispatcher;
-import javax.servlet.ServletContext;
+import javax.ejb.EJB;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.airline.models.FlightClass;
 import com.airline.models.Gender;
 import com.airline.models.Passenger;
+import com.airline.service.PassengerService;
 
 /**
- * Servlet implementation class AddPassenger
+ * Servlet implementation class AddPassenger2
  */
-@WebServlet("/AddPassenger")
+@WebServlet("/AddPassenger2")
 public class AddPassenger extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+     
+	@EJB
+	PassengerService ps;
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -38,111 +37,37 @@ public class AddPassenger extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		Passenger p = new Passenger();
 		
-		request.setAttribute("first_name", "");
-		request.setAttribute("last_name", "");
-		request.setAttribute("dob", "");
+		p.setFirstName("Pavel");
+		p.setLastName("Rogozhin");
 		
-		RequestDispatcher view = request.getRequestDispatcher("WEB-INF/views/add_passenger.jsp");
-		view.forward(request, response);
+		Calendar cal = Calendar.getInstance();
+		
+		cal.set(Calendar.YEAR, 1982);
+		cal.set(Calendar.MONTH, 10);
+		cal.set(Calendar.DAY_OF_MONTH, 5);
+		
+		Date dob = cal.getTime();
+		
+		p.setDob(dob);
+		
+		p.setGender(Gender.Male);
+		
+		p.setFlightClass(FlightClass.Coach);
+		
+		System.out.println(p);
+		
+		ps.addPassenger(p);
+		
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-		request.setAttribute("errors", false);
-		
-		Passenger p = new Passenger();
-		
-		String firstName = request.getParameter("first-name");
-		System.out.println("firstName: " + firstName);
-		
-		if(firstName.length() == 0){
-			System.out.println("empty first error");
-			request.setAttribute("errors", true);
-			request.setAttribute("firstName_errors", true);
-			request.setAttribute("first_name", "");
-		}
-		else {
-			p.setFirstName(firstName);
-			request.setAttribute("first_name", firstName);
-		}
-		
-		String lastName = request.getParameter("last-name");
-		System.out.println("lastName: " + lastName);
-		
-		if(lastName.length() == 0){
-			System.out.println("empty last error");
-			request.setAttribute("errors", true);
-			request.setAttribute("lastName_errors", true);
-			request.setAttribute("last_name", "");
-			
-		}
-		else {
-			p.setLastName(lastName);
-			request.setAttribute("last_name", lastName);
-			request.setAttribute("last_name", lastName);
-		}
-		
-		String dob_raw = request.getParameter("dob");
-		
-		String dobArray [] = dob_raw.split("\\/");
-		
-		String pattern = "^\\d{1,2}\\/\\d{1,2}\\/\\d{4}$";
-		Pattern r = Pattern.compile(pattern);
-		
-		Matcher m = r.matcher(dob_raw);
-		
-		if(m.find()){
-			
-			String month = dobArray[0];
-			String day = dobArray[1];
-			String year = dobArray[2];
-			
-			Calendar cal = Calendar.getInstance();
-			
-			cal.set(Calendar.YEAR, Integer.parseInt(year));
-			cal.set(Calendar.MONTH, Integer.parseInt(month));
-			cal.set(Calendar.DAY_OF_MONTH, Integer.parseInt(day));
-			
-			Date dob = cal.getTime();
-			
-			System.out.println(dob);
-			
-			p.setDob(dob);
-			request.setAttribute("dob", dob_raw);
-			
-		}
-		else {
-			System.out.println("Invalid date of birth");
-			request.setAttribute("errors", true);
-			request.setAttribute("date_format_error", true);
-			request.setAttribute("dob", dob_raw);
-			if(dob_raw.length() == 0){
-				request.setAttribute("dob", "");
-			}
-		}
-		
-		String gender = request.getParameter("gender");
-		System.out.println("gender: " + gender);
-		p.setGender(Gender.valueOf(gender));
-		
-		if ((Boolean) request.getAttribute("errors")){
-		    RequestDispatcher view = request.getRequestDispatcher("WEB-INF/views/add_passenger.jsp");
-
-		    view.forward(request, response);
-		}
-		else {
-			ServletContext sc = this.getServletContext();
-			synchronized (this){ // only one thread at a time make changes
-			ArrayList<Passenger> pList = (ArrayList<Passenger>)sc.getAttribute("passengers");
-			pList.add(p);
-			sc.setAttribute("passengers", pList);
-			}
-			response.sendRedirect("");
-		}
+		// TODO Auto-generated method stub
 	}
 
 }
